f8ac59462f73ea5fe5b9cd39f162209ffe7c6547
OvanessTea <OvanessTea@yandex.com>
2025-06-29 21:21:44 +0200

    update search component

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -9,26 +9,30 @@ interface SearchProps {
     onSubmit: (search?: string) => void;
 }
 
-export const Search = ({onSubmit}: SearchProps) => {
+export const Search = ({ onSubmit }: SearchProps) => {
     const [search, setSearch] = useState("");
+
+    const handleSearch = (search?: string) => {
+        onSubmit(search);
+    };
 
     return (
         <div className={styles.search}>
             <TextInput
                 className={styles.input}
                 placeholder="Search for matches"
-                leftSection={<IconSearch cursor="pointer" size={16} onClick={() => onSubmit(search)}/>}
+                leftSection={<IconSearch cursor="pointer" size={16} onClick={() => handleSearch(search)} />}
                 rightSection={search.length > 0 && (
                     <IconX cursor="pointer" size={16} onClick={() => {
                         setSearch('');
-                        onSubmit();
+                        handleSearch();
                     }} />
                 )}
                 value={search}
-                onChange={(e) => setSearch(e.target.value)} 
+                onChange={(e) => setSearch(e.target.value)}
                 onKeyDown={(e) => {
                     if (e.key === 'Enter') {
-                        onSubmit(search);
+                        handleSearch(search);
                     }
                 }}
             />
