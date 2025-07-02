1d4aeab5b47f37908c4cf78759000be2b1060825
OvanessTea <OvanessTea@yandex.com>
2025-07-02 17:51:13 +0200

    remove unused next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,3 @@
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://h49q2nbrwe.execute-api.eu-central-1.amazonaws.com/HiringTest/:path*',
-      },
-    ];
-  },
+  
 };
